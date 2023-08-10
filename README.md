efd84bdb6ddf1c0639b3fc6f8ecaa3f7d8752d4b
Miguel Angel Pérez Contreras <mapcsasystem@gmail.com>
2023-08-09 22:08:38 -0600

    build docs

--- README.md
+++ README.md
@@ -10,9 +10,9 @@ La Muestra el funcionamiento de los pipes [Pipes](https://angular.io/api?query=p
 
 #### Prime NG [Prime NG](https://primeng.org/installation) version 16.1.0.
 
-### Prime Icons [Prime Icons](https://primeng.org/icons)
+### Prime Icons [Prime Icons](https://primeng.org/icons) v6.0.1
 
-#### Prime Flex [Prime Flex](https://www.primefaces.org/primeflex/)
+#### Prime Flex [Prime Flex](https://www.primefaces.org/primeflex/) version 3.3.1
 
 #### NodeJS [Node](https://nodejs.org/dist/v18.16.0/) version 18.16.0.
 
@@ -24,7 +24,34 @@ La Muestra el funcionamiento de los pipes [Pipes](https://angular.io/api?query=p
 
 #### GitHub-Pages [GitHub-Pages](https://pages.github.com/)
 
-[Repo](https://github.com/mapcsasystem/pipesApp.git)
+## Requerimientos para correr la app
+
+#### Instalar NodeJS para el Sistema Operativo a utilizar
+
+##### "window abrir cmd como administrador"
+
+```
+npm i -g @angular/cli
+```
+
+##### "linux ó MacOs"
+
+```
+sudo npm i -g @angular/cli
+```
+
+### Comandos a utilizar
+
+```
+git clone https://github.com/mapcsasystem/pipesApp.git
+cd pipesApp
+npm install
+npm start
+```
+
+#### Pagina web de la app [Web Countries App](https://mapcsasystem.github.io/pipesApp/)
+
+El diseño web repositive
 
 This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 16.2.0.
 
--- src/app/shared/components/menu/menu.component.ts
+++ src/app/shared/components/menu/menu.component.ts
@@ -1,42 +1,46 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { MenuItem } from 'primeng/api';
 
 @Component({
   selector: 'shared-menu',
   templateUrl: './menu.component.html',
   styleUrls: ['./menu.component.scss'],
 })
-export class MenuComponent {
-  menuItems = [
-    {
-      label: 'Pipes de Angular',
-      icon: 'pi pi-desktop',
-      items: [
-        {
-          label: 'Textos y Fechas',
-          icon: 'pi pi-align-left',
-          routerLink: '/',
-        },
-        {
-          label: 'Números',
-          icon: 'pi pi-dollar',
-          routerLink: 'numbers',
-        },
-        {
-          label: 'No comunes',
-          icon: 'pi pi-globe',
-          routerLink: 'uncommon',
-        },
-      ],
-    },
-    {
-      label: 'Pipes personalizados',
-      icon: 'pi pi-cog',
-      items: [
-        {
-          label: 'Otro elemento',
-          icon: 'pi pi-cog',
-        },
-      ],
-    },
-  ];
+export class MenuComponent implements OnInit {
+  menuItems: MenuItem[] = [];
+  ngOnInit(): void {
+    this.menuItems = [
+      {
+        label: 'Pipes de Angular',
+        icon: 'pi pi-desktop',
+        items: [
+          {
+            label: 'Textos y Fechas',
+            icon: 'pi pi-align-left',
+            routerLink: '/',
+          },
+          {
+            label: 'Números',
+            icon: 'pi pi-dollar',
+            routerLink: 'numbers',
+          },
+          {
+            label: 'No comunes',
+            icon: 'pi pi-globe',
+            routerLink: 'uncommon',
+          },
+        ],
+      },
+      {
+        label: 'Pipes personalizados',
+        icon: 'pi pi-cog',
+        items: [
+          {
+            label: 'Otro elemento',
+            icon: 'pi pi-cog',
+          },
+        ],
+      },
+    ];
+  }
 }
